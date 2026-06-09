47150014105bcaab419f619dfb3be14814c18154
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2026-06-09 22:09:49 +0500

    20260609_component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import CompFun from './CompFun'
 
 function App() {
  
@@ -8,6 +9,8 @@ function App() {
     <>
    <h1>Learn react</h1>
    <h2>By : Muhammad Jumshaid</h2>
+
+   <CompFun/>
    </>
   )
 }
---
+++ src/CompFun.jsx
@@ -0,0 +1,6 @@
+function CompFun(){
+    return(
+        <h1>Good Work</h1>
+    )
+}
+export default CompFun;
