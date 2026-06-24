fa6c01e74c6219cafd87ea2ce15811e4d5439376
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2026-06-24 23:05:07 +0500

    20260624_Passing Object as Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 import './App.css'
+import ObjectProps from './ObjectProps'
 // import Assignment_3 from './Assignment_3'
-import PropsInReact from './PropsInReact'
+// import PropsInReact from './PropsInReact'
 // import Assignment2 from './Assignment2'
 // import ToggleButton from './ToggleButton'
 // import Assignment1 from './Assignment1.jsx'
@@ -12,8 +13,26 @@ import PropsInReact from './PropsInReact'
 // import CompFun, { flower, Provinces } from './CompFun'
 
 function App() {
- let myName = "Muhammad Jumshaid"
-
+//  let myName = "Muhammad Jumshaid"
+ 
+const employee = {
+  empName : "Jumshaid", 
+  empSalary : 12000, 
+  empAge : 25, 
+  empMail : "jimi@gmail.com"
+}
+const employee2 = {
+  empName : "Jumshaid", 
+  empSalary : 12000, 
+  empAge : 25, 
+  empMail : "jimi@gmail.com"
+}
+const employee3 = {
+  empName : "Jimi", 
+  empSalary : 12000, 
+  empAge : 25, 
+  empMail : "jimi@gmail.com"
+}
   return (
     <>
    <h1 style={{color : 'chocolate' }}>Learn react (with) Muhammad Jumshaid</h1>
@@ -29,8 +48,11 @@ function App() {
 {/* <ToggleButton/> */}
 {/* <Assignment_3/> */}
 {/* <PropsInReact myName="Jumshaid"/> */}
-<PropsInReact myName={myName} age ={25} mail="jimikhan.1222@gmail.com" addrs= {"Mohalla Sherani wala"}/>
+{/* <PropsInReact myName={myName} age ={25} mail="jimikhan.1222@gmail.com" addrs= {"Mohalla Sherani wala"}/> */}
 
+<ObjectProps user = {employee}/>
+<ObjectProps user = {employee2}/>
+<ObjectProps user = {employee3}/>
    </>
   )
 }
---
+++ src/ObjectProps.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+const ObjectProps = ({user}) => {
+  return (
+    <div>
+      <h1>Object and Props</h1>
+      {/* <h2>{user.empName} </h2>
+      <h2>{user.empAge}</h2> */}
+      <h2>{user.empName} </h2>
+      <h2>{user.empAge}</h2>
+    </div>
+  )
+}
+
+export default ObjectProps
