f1f4ad6015fc34b5a7e7884f5fbd104e3aeb2ec7
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2026-08-04 15:20:33 +0500

    modified code

--- src/TableAndArray.jsx
+++ src/TableAndArray.jsx
@@ -1,76 +1,71 @@
-import React from 'react'
-
 const TableAndArray = () => {
 
     const employeeInf = [
         {
-            Id : 1,
-            "Name" : "Jumshaid",
-            Salary : 25000,
-            Age : 24,
-            Email : "Jumshaid@gmail.com"
+            Id: 1,
+            "Name": "Jumshaid",
+            Salary: 25000,
+            Age: 24,
+            Email: "Jumshaid@gmail.com"
         },
         {
-            Id : 2,
-            "Name" : "Kamran",
-            Salary : 20000,
-            Age : 34,
-            Email : "Kamran@gmail.com"
+            Id: 2,
+            "Name": "Kamran",
+            Salary: 20000,
+            Age: 34,
+            Email: "Kamran@gmail.com"
         },
         {
-            Id : 3,
-            "Name" : "Zohaib",
-            Salary : 50000,
-            Age : 30,
-            Email : "Zohaib@gmail.com"
+            Id: 3,
+            "Name": "Zohaib",
+            Salary: 50000,
+            Age: 30,
+            Email: "Zohaib@gmail.com"
         },
         {
-            Id : 4,
-            "Name" : "Ali",
-            Salary : 34000,
-            Age : 29,
-            Email : "Ali@gmail.com"
+            Id: 4,
+            "Name": "Ali",
+            Salary: 34000,
+            Age: 29,
+            Email: "Ali@gmail.com"
         },
     ]
-    
-  return (
-    <div>
-      <h1>Array Data in Table</h1>
 
-      {/* <table border={{border : 1}}> */}
-      <table border="1">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Salary</th>
-                <th>Age</th>
-                <th>Email</th>
-            </tr>
-        </thead>
-        <tbody>
-                
-            {
-                employeeInf.map((item)=>{
-            return(
-                    <>
+    return (
+        <div>
+            <h1>Array Data in Table</h1>
+
+            {/* <table border={{border : 1}}> */}
+            <table border="1">
+                <thead>
                     <tr>
-                <td>{item.Id}</td>
-                <td>{item.Name}</td>
-                <td>{item.Salary}</td>
-                <td>{item.Age}</td>
-                <td>{item.Email}</td>
-                </tr>
-            </>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Salary</th>
+                        <th>Age</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
 
-           )
-            })
-        }
-            
-        </tbody>
-      </table>
-    </div>
-  )
+                    {
+                        employeeInf.map(user => 
+                            (
+                           <tr>
+                                <td>{user.Id}</td>
+                                <td>{user.Name}</td>
+                                <td>{user.Salary}</td>
+                                <td>{user.Age}</td>
+                                <td>{user.Email}</td>
+                            </tr>
+                            )
+                        )
+                    }
+
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default TableAndArray
